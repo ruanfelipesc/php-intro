b3b0cf8b4de98b462974b5ef70abbdc5276a7429
Ruan <ruanfeliperoliveira@gmail.com>
2022-04-23 11:33:40 -0300

    adicionando validação de campos do formulario

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
-<?php
-
-$categorias = [];
-$categorias[] = 'infantil';
-$categorias[] = 'adolescente';
-$categorias[] = 'adulto';
-$categorias[] = 'idoso';
-//print_r($categorias);
-
-$nome = 'Ruan';//string
-$idade = 8;//integer
-
-//var_dump($nome);
-//var_dump($idade);
-
-if($idade >=6 && $idade <=12)
-{
-   for($i = 0; $i <= count($categorias); $i++)
-   {
-        if($categorias[$i] == 'infantil')
-            echo 'O nadador ' . $nome . " compete na categoria " . $categorias[$i];
-   }
-}
-else if($idade >= 13 && $idade <=18)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-   {
-        if($categorias[$i] == 'adolescente')
-            echo 'O nadador ' . $nome . " compete na categoria ". $categorias[$i];
-   }
-}
-else
-{
-    for($i = 0; $i <= count($categorias); $i++)
-   {
-        if($categorias[$i] == 'adulto')
-            echo 'O nadador ' . $nome . " compete na categoria " . $categorias[$i];
-   }
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <title>Formulário de inscrição</title>
+    <meta name= "author" content="">
+    <meta name= "description" content="">
+    <meta name= "viewport" content="width-device-width, initial-scale=1">
+</head>
+<body>
+    <p> FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+<form action="script.php" method="post">
+    <p>Your name: <input type="text" name="nome" /></p>
+    <p>Your age: <input type="text" name="idade" /></p>
+    <p><input type="submit" value="Enviar dados do competidor"/></p>
+</form>
+</body>
+</html>
